c0de128ad66fefb4e9b2ed5271d34e84e809ba82
gaurav147-star <gauravgupta9158@gmail.com>
2022-04-19 12:30:02 +0530

    COA-assignment-9-12.asm

---
+++ COA-Lab/12.asm
@@ -0,0 +1,40 @@
+;program to print two string in two lines
+org 100h
+.model small
+
+.data
+ var1 db 'myself gaurav gupta$'
+ var2 db 'from it branch$'
+.code
+main proc
+
+mov ax ,@data
+mov ds ,ax
+mov dx, offset var1
+mov ah,9
+int 21h
+
+call enterkey
+
+mov dx, offset var2
+mov ah,9
+int 21h
+
+mov ah,4ch
+int 21h
+
+main endp
+
+enterkey proc
+mov dx,13
+mov ah,2
+int 21h
+
+mov dx,10
+mov ah,2
+int 21h
+
+ret
+enterkey endp
+
+end main
